f3a3e870b2d0769e01bf96bbd8ca69db8fc13f49
larsbabs <lars@iraca.nl>
2021-04-13 10:42:25 +0200

    api

--- Proftaak/api/eigen api/eigen-api.py
+++ Proftaak/api/eigen api/eigen-api.py
@@ -7,5 +7,8 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+@app.route('/babs/', methods=['GET'])
+def babs():
+    return "<h1>big nigga</h1>"
 
-app.run()
+app.run(host="0.0.0.0")
