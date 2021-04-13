4f14aceb20536eff6edf4799013b7a8be96d35a5
larsbabs <lars@iraca.nl>
2021-04-13 10:59:53 +0200

    api

--- Proftaak/api/eigen api/eigen-api.py
+++ Proftaak/api/eigen api/eigen-api.py
@@ -2,14 +2,14 @@ import flask
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
+books = open("./books.json")
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 @app.route('/api/v1/books/test/', methods=['GET'])
 def babs():
-    return ./books.json
+    return books.read()
 
 pull test ubuntu
 
