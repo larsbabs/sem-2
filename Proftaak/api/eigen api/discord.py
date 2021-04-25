f558232a24c0e3a1a5ef9fcbe0754c27c864dd4d
larsbabs <lars@iraca.nl>
2021-04-25 23:00:41 +0200

    discord bot

---
+++ Proftaak/api/eigen api/discord.py
@@ -0,0 +1,19 @@
+API_ENDPOINT = 'https://discord.com/api/v8'
+CLIENT_ID = '332269999912132097'
+CLIENT_SECRET = '937it3ow87i4ery69876wqire'
+REDIRECT_URI = 'https://nicememe.website'
+
+def exchange_code(code):
+  data = {
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+    'grant_type': 'authorization_code',
+    'code': code,
+    'redirect_uri': REDIRECT_URI
+  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  r = requests.post('%s/oauth2/token' % API_ENDPOINT, data=data, headers=headers)
+  r.raise_for_status()
+  return r.json()
---
+++ discord/bot.py
@@ -0,0 +1,78 @@
+# bot.py
+import os
+import random
+import requests
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = "ODIzODkzNTQ4MzY1MzgxNzEy.YFncqQ.GPwMT8BTM7aCRYSzMDGoVlJOjas"
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+@bot.command(name='?')
+async def nine_nine(ctx):
+
+    help_text = """    "embed": {
+    "title": "title ~~(did you know you can have markdown here too?)~~",
+    "description": "this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```",
+    "url": "https://discordapp.com",
+    "color": 6053045,
+    "timestamp": "2021-04-23T14:06:39.796Z",
+    "footer": {
+      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+      "text": "footer text"
+    },
+    "thumbnail": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "image": {
+      "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "author": {
+      "name": "author name",
+      "url": "https://discordapp.com",
+      "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+    },
+    "fields": [
+      {
+        "name": "🤔",
+        "value": "some of these properties have certain limits..."
+      },
+      {
+        "name": "😱",
+        "value": "try exceeding some of them!"
+      },
+      {
+        "name": "🙄",
+        "value": "an informative error should show up, and this view will remain as-is until all issues are fixed"
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "these last two",
+        "inline": true
+      },
+      {
+        "name": "<:thonkang:219069250692841473>",
+        "value": "are inline fields",
+        "inline": true
+      }
+    ]
+  }"""
+    await ctx.send(str(help_text))
+bot.run(TOKEN)
