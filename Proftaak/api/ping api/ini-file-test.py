f6b1e302fa891633fb428aeb43f68af3ee2b7da7
larsbabs <lars@iraca.nl>
2021-05-02 01:12:40 +0200

    ping api with .ini file

--- Proftaak/api/ping api/ini-file-test.py
+++ Proftaak/api/ping api/ini-file-test.py
@@ -7,7 +7,22 @@ import configparser
 config = configparser.ConfigParser()
 config.read(r"C:\Users\larsi\Documents\github\Proftaak\api\ping api\ip-config.ini")
 
-#print(config['ping-ip'].items)
+# Maak een list van de namen in de .ini file
+def nameList():
+    name_list = []
+    for key in config['ping-ip']:
+        name_list.append(key)
+    return name_list
 
-for key in config['ping-ip']:  
-    print(key)
+# Maak een list van de IP's
+def ipList():
+    count = 0
+    ip_list = []
+    while True:
+        ip_list.append(config['ping-ip'][nameList()[count]])
+        count += 1
+        if count == len(nameList()):
+            break
+    return ip_list
+print(nameList)
+print(ipList())
